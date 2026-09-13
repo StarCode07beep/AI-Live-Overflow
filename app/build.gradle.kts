4f75c2f0803e289f427c738233d86e2bd1304b6f
StarCode07beep <1036475259@qq.com>
2026-09-13 18:14:11 +0800

    chore(deskpet): 1.1-notify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0-power"
+        versionName = "1.1-notify"
     }
 
     signingConfigs {
