4dd7d1aacfea1317dce02bf6fab0838629888e71
StarCode07beep <1036475259@qq.com>
2026-09-15 23:47:14 +0800

    chore(deskpet): 1.5-sleep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.starcode.xujin_deskpet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.8-onenap"
+        versionCode = 5
+        versionName = "1.9-static"
     }
 
     signingConfigs {
